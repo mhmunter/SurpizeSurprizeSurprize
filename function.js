95ec6c998be796694b7624e0fcca50d122fde801
M Munter <mark.munter1@gmail.com>
2022-02-26 19:42:49 -0600

    first step

---
+++ function.js
@@ -0,0 +1,18 @@
+var starButton = document.getElementById("starButton")
+
+starButton.addEventListener("click", startFunction)
+
+function startFunction(){
+    console.log("started.")
+    starButton.classList.add("hide")
+}
+
+
+
+
+
+
+
+
+
+
