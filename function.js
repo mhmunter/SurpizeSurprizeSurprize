2d254cd85f7630f5e7b61d68df554af4f98650c3
M Munter <mark.munter1@gmail.com>
2022-03-03 09:26:13 -0600

    Revert "added css timer and started timer function"
    This reverts commit aa4c1fd7607f18ee4efaae468659def534acdf8a.

--- function.js
+++ function.js
@@ -1,59 +1,18 @@
-var startButton = document.getElementById("startButton")
-var timerEl = document.getElementById('countdown');
+var starButton = document.getElementById("starButton")
 
-// var timer = document.querySelector('h3')
-
-
+starButton.addEventListener("click", startFunction)
 
 function startFunction(){
     console.log("started.")
-    startButton.classList.add("hide")
-// document.getElementById('countdown');
-
+    starButton.classList.add("hide")
 }
 
-startButton.addEventListener("click", startFunction);
-
-// function countdown(){
-//     var timeLeft = 10;
-//     var timeInterval = setInterval(function() {
-//         timerEl.textContent =timeLeft;
-//         startTime--;
-//         if(startTime  === 0){
-//           clearInterval(timeInterval);
-//         //   displayMessage();
-//         console.log("times up!!!")
-//         }
-//       } ,1000 );
-
-// }
-// function countdown(){
-//     var timeLeft = timer;
-//     var timeInterval =  function setInterval(){
-// timeLeft--;
-// timerEl.textContent = timeLeft;
-// if (timeLeft === 0) {
-//     console.log('Game Over man!')
-// }1000;
-//     } 
-// }
 
 
 
 
-function countdown() {
-    var timeLeft = 5;
-  
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function() {
-      timerEl.textContent =timeLeft;
-      timeLeft--;
-      if(timeLeft  === 0){
-        clearInterval(timeInterval);
-        displayMessage();
-      }
-    } ,1000 );
-  }
+
+
 
 
 
