799357a0a3dcb185e5b3b6e97a36451d3a1788b4
M Munter <mark.munter1@gmail.com>
2022-03-05 14:57:33 -0600

    chanded radio to buttons changing object to correct properties

--- function.js
+++ function.js
@@ -23,11 +23,30 @@ function countdown() {
     }
     
   } ,1000 );
+  // end timer message-------------------
+var delay = 6000; // delay time in milliseconds
+
+var timeoutId = setTimeout(function () {
+    alert('Times up partner!');
+}, delay);
 }
 
 
 document.getElementById("startButton").addEventListener("click" , countdown);
 
+var buttonNext = document.querySelector("#startButton");
+console.log("yupe");
+
+
+
+
+// answer arry chec
+// var answer = [answerAe1.value, answerBe1.value, answersCe1, answerDe1.value];
+
+
+
+
+// next button event lsitener if checked = 
 
 
 
@@ -38,6 +57,35 @@ document.getElementById("startButton").addEventListener("click" , countdown);
 
 
 
+// var taskItemEl = document.createElement("li");
+// taskItemEl.textContent = "hello";
+// tasksToDoEl.appendChild(taskItemEl);
+// taskItemEl.className = "task-item";
+
+// var buttonEl = document.querySelector("#save-task");
+// var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// buttonEl.addEventListener("click", function() {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEl.appendChild(listItemEl);
+// });
+
+
+/*{ <button id="btnToClick">Click Me</button>
+<script>
+    "use strict";
+    (function() {
+        var btnToClick = document.getElementById('btnToClick');
+
+        console.log(btnToClick); // prints <button id="btnToClick">Click Me</button>
+    })();
+</script> }*/
+
+
+// var mainHeading = document.getElementById('main-heading');
+// mainHeading.innerHTML = "Hello Codeup!";
 
 
 
@@ -45,75 +93,53 @@ document.getElementById("startButton").addEventListener("click" , countdown);
 
 
 
+var answers = [
+  {Question: "Question 1: What is the best show on TV?",
+      options:{
+          A: 'A) BattleStar',
+          B: 'B) Daily Show',
+          C: 'C) Old Reruns',
+          D: 'D) Golden Girls'
+  }},
+  {Question: "Question 2: Who had the strangest empire?",
+      options:{
+          A: 'A) Rome',
+          B: 'B) Mongolia',
+          C: 'C) Kush',
+          D: 'D) Aztec'
 
-// function countdown() {
-//   var timeLeft = 5;
+      }},
+  {Question: "Question 3: Who wrote my favorite books?",
+      options:{
+        A: 'A) Preston',
+        B: 'B) King',
+        C: 'C) Koontz',
+        D: 'D) William'
+  }},
+  {Question: "Question 4: Life According to Mr.Munter",
+  options:{
+    A: 'A) Life is good.',
+    B: 'B) Life is bad.',
+    C: 'C) life is ok.',
+    D: 'D) life is MEh.'
 
-//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var timeInterval = setInterval(function () {
-//     // As long as the `timeLeft` is greater than 1
-//     if (timeLeft > 1) {
-//       // Set the `textContent` of `timerEl` to show the remaining seconds
-//       timerEl.textContent = timeLeft + ' seconds remaining';
-//       // Decrement `timeLeft` by 1
-//       timeLeft--;
-//     } else  (timeLeft === 1) {
-//       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//       timerEl.textContent = timeLeft + ' second remaining';
-//       timeLeft--;
-//     } 
-//   }, 1000);
-// }
+      }},
+  {Question: "Question 5: Who wrote my favorite books?",
+  options:{
+    A: 'A) Preston',
+    B: 'B) King',
+    C: 'C) Koontz',
+    D: 'D) William'
+      }
 
-// function startFunction(){
-//     console.log("started.")
-//     startButton.classList.add("hide")
-// document.getElementById('countdown');
-
-// }
-
-// startButton.addEventListener("click", startFunction);
+  }
 
 
-// function countdown() {
-  
+  ];
 
-//   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var timeInterval = setInterval(function() {
-//     var timeLeft = 5;
-//     timerEl.textContent =timeLeft;
-//     timeLeft--;
-//     if(timeLeft  === 0){
-//       clearInterval(timeInterval);
-//       displayMessage();
-//     }
-//   } ,1000 );
-// }
-// countdown();
 
-// // function countdown(){
-// //     var timeLeft = 10;
-// //     var timeInterval = setInterval(function() {
-// //         timerEl.textContent =timeLeft;
-// //         startTime--;
-// //         if(startTime  === 0){
-// //           clearInterval(timeInterval);
-// //         //   displayMessage();
-// //         console.log("times up!!!")
-// //         }
-// //       } ,1000 );
 
-// // }
-// // function countdown(){
-// //     var timeLeft = timer;
-// //     var timeInterval =  function setInterval(){
-// // timeLeft--;
-// // timerEl.textContent = timeLeft;
-// // if (timeLeft === 0) {
-// //     console.log('Game Over man!')
-// // }1000;
-// //     } 
-// // }
+
 
 
 
